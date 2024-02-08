d4171367b4c4a9668fd4bb8132300370c4ad4042
Chris Lubomanski <lubomanski@gmail.com>
2024-02-08 15:06:24 +0100

    Remove mason

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,16 @@
 return {
   {
-	  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "lua", "vim", "vimdoc", "ruby", "terraform", "go" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
   }
 }
